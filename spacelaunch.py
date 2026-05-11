8a11fdb88f296ca686deddde94100cdf0d38d516
jt-codespace <jdtailor2130@gmail.com>
2026-05-11 21:36:49 +0530

    Update spacelaunch.py

--- spacelaunch.py
+++ spacelaunch.py
@@ -1,18 +1,30 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# Space launch dataset
 data = {
-    "Country": ["USA", "India", "China", "Russia"],
-    "Launches": [120, 35, 80, 60]
+    "Country": ["USA", "India", "China", "Russia", "India"],
+    "Launches": [120, 35, None, 60, 35]
 }
-
 
 df = pd.DataFrame(data)
 
+print("Original Data")
 print(df)
 
-plt.bar(df["Country"],df["Launches"])
+# Remove duplicate rows
+df = df.drop_duplicates()
+
+# Fill missing values with 0
+df["Launches"] = df["Launches"].fillna(0)
+
+print("\nCleaned Data")
+print(df)
+
+plt.bar(df["Country"], df["Launches"])
+
 plt.title("Space Launch Analysis")
 plt.xlabel("Country")
-plt.ylabel("Number Of Launches")
+plt.ylabel("Number of Launches")
+
 plt.show()
